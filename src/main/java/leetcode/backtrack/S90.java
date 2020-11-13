a2e128da03fecf2ad49949a5a27cb6998638c9b4
lianghui <lianghui@efun.com>
2020-11-13 18:40:16 +0800

    回溯3

--- src/main/java/leetcode/backtrack/S90.java
+++ src/main/java/leetcode/backtrack/S90.java
@@ -27,11 +27,25 @@ public class S90 {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
         Arrays.sort(nums);
-        boolean[] visited = new boolean[nums.length];
-        for (int i = 0; i <= nums.length; i++) {
-            dfs(ans, path, visited, nums, 0, i);
-        }
+        getAns(nums, 0, path, ans);
+//        boolean[] visited = new boolean[nums.length];
+//        for (int i = 0; i <= nums.length; i++) {
+//            dfs(ans, path, visited, nums, 0, i);
+//        }
         return ans;
+    }
+
+    private void getAns(int[] nums, int start, List<Integer> path, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(path));
+
+        for (int i= start; i < nums.length; i++){
+            if (i > start && nums[i] == nums[i-1]){
+                continue;//和上个数字相等就跳过
+            }
+            path.add(nums[i]);
+            getAns(nums, i + 1, path, ans);
+            path.remove(path.size() - 1);
+        }
     }
 
     private void dfs(List<List<Integer>> ans, List<Integer> path, boolean[] visited,
@@ -43,7 +57,7 @@ public class S90 {
         }
         for (int i = cur; i < nums.length; i++) {
             if (visited[i] || (i > 0 && nums[i] == nums[i - 1]) && !visited[i-1]) {
-                continue;
+                continue;//和上个数字相等就跳过
             }
             path.add(nums[i]);
             visited[i] = true;
@@ -54,10 +68,34 @@ public class S90 {
 
     }
 
+    public List<List<Integer>> subsetsWithDup1(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());//初始化空数组
+        Arrays.sort(nums);
+        int start = 1;//保存新鲜的开始位置
+        for (int i = 0; i < nums.length; i++){
+            List<List<Integer>> ans_tmp = new ArrayList<>();
+//            遍历之前所有结果
+            for (int j = 0; j < ans.size(); j++){
+                List<Integer> list = ans.get(j);
+//                如果出现重复数字，就跳过所有旧解
+                if (i > 0 && nums[i] == nums[i-1] && j < start){
+                    continue;
+                }
+                List<Integer> tmp = new ArrayList<>(list);
+                tmp.add(nums[i]);//加入新增数字
+                ans_tmp.add(tmp);
+            }
+            start = ans.size();//更新新解的开始位置
+            ans.addAll(ans_tmp);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         S90 sou = new S90();
-        int[] nums = {1, 2, 2};
-        List<List<Integer>> ans = sou.subsetsWithDup(nums);
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> ans = sou.subsetsWithDup1(nums);
         for (List<Integer> tmp : ans) {
             System.out.println(tmp);
         }
---
+++ src/main/java/leetcode/backtrack/S93.java
@@ -0,0 +1,112 @@
+package leetcode.backtrack;
+//给定一个只包含数字的字符串，复原它并返回所有可能的 IP 地址格式。
+//
+// 有效的 IP 地址 正好由四个整数（每个整数位于 0 到 255 之间组成，且不能含有前导 0），整数之间用 '.' 分隔。
+//
+// 例如："0.1.2.201" 和 "192.168.1.1" 是 有效的 IP 地址，但是 "0.011.255.245"、"192.168.1.312"
+// 和 "192.168@1.1" 是 无效的 IP 地址。
+//
+//
+//
+// 示例 1：
+//
+// 输入：s = "25525511135"
+//输出：["255.255.11.135","255.255.111.35"]
+//
+//
+// 示例 2：
+//
+// 输入：s = "0000"
+//输出：["0.0.0.0"]
+//
+//
+// 示例 3：
+//
+// 输入：s = "1111"
+//输出：["1.1.1.1"]
+//
+//
+// 示例 4：
+//
+// 输入：s = "010010"
+//输出：["0.10.0.10","0.100.1.0"]
+//
+//
+// 示例 5：
+//
+// 输入：s = "101023"
+//输出：["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= s.length <= 3000
+// s 仅由数字组成
+//
+// Related Topics 字符串 回溯算法
+// 👍 454 👎 0
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class S93 {
+    static final int SEG_COUNT = 4;
+    List<String> ans = new ArrayList<String>();
+    int[] segments = new int[SEG_COUNT];
+
+    public List<String> restoreIpAddresses(String s) {
+        segments = new int[SEG_COUNT];
+        dfs(s, 0, 0);
+        return ans;
+    }
+
+    public void dfs(String s, int segId, int segStart) {
+        // 如果找到了 4 段 IP 地址并且遍历完了字符串，那么就是一种答案
+        if (segId == SEG_COUNT) {
+            if (segStart == s.length()) {
+                StringBuffer ipAddr = new StringBuffer();
+                for (int i = 0; i < SEG_COUNT; ++i) {
+                    ipAddr.append(segments[i]);
+                    if (i != SEG_COUNT - 1) {
+                        ipAddr.append('.');
+                    }
+                }
+                ans.add(ipAddr.toString());
+            }
+            return;
+        }
+
+        // 如果还没有找到 4 段 IP 地址就已经遍历完了字符串，那么提前回溯
+        if (segStart == s.length()) {
+            return;
+        }
+
+        // 由于不能有前导零，如果当前数字为 0，那么这一段 IP 地址只能为 0
+        if (s.charAt(segStart) == '0') {
+            segments[segId] = 0;
+            dfs(s, segId + 1, segStart + 1);
+        }
+
+        // 一般情况，枚举每一种可能性并递归
+        int addr = 0;
+        for (int segEnd = segStart; segEnd < s.length(); ++segEnd) {
+            addr = addr * 10 + (s.charAt(segEnd) - '0');
+            if (addr > 0 && addr <= 0xFF) {
+                segments[segId] = addr;
+                dfs(s, segId + 1, segEnd + 1);
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        S93 sou = new S93();
+        int[] nums = {1, 2, 3};
+    }
+}
